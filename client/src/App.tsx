af2ab5434e37d55a00eefb0b6514ae9aa5ea3950
friedrice001-f <friedrice057@gmail.com>
2026-07-31 12:03:00 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import { ThemeProvider } from "./context/ThemeContext";
 import { ChatProvider, useChat } from "./context/ChatContext";
+import { CallProvider } from "./context/CallContext";
 import { LoginScreen } from "./components/Login/LoginScreen";
 import { AppShell } from "./components/Layout/AppShell";
+import { IncomingCallModal } from "./components/Call/IncomingCallModal";
+import { ActiveCallScreen } from "./components/Call/ActiveCallScreen";
 
 function Root() {
   const { currentUser } = useChat();
@@ -12,7 +15,11 @@ export default function App() {
   return (
     <ThemeProvider>
       <ChatProvider>
-        <Root />
+        <CallProvider>
+          <Root />
+          <IncomingCallModal />
+          <ActiveCallScreen />
+        </CallProvider>
       </ChatProvider>
     </ThemeProvider>
   );
