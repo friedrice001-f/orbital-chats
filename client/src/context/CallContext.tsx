a8889c61316c22d21f4a399718dc493bd0473c92
friedrice001-f <friedrice057@gmail.com>
2026-08-02 15:43:31 +0000

    Switch to working Metered TURN server

--- client/src/context/CallContext.tsx
+++ client/src/context/CallContext.tsx
@@ -39,22 +39,26 @@ const CallContext = createContext<CallContextValue | null>(null);
 
 const ICE_SERVERS: RTCConfiguration = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun.relay.metered.ca:80" },
     {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: "turn:global.relay.metered.ca:80",
+      username: "ea791fc601b5a9a0521404a8",
+      credential: "y83LlUrGQehWPEgL",
     },
     {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "ea791fc601b5a9a0521404a8",
+      credential: "y83LlUrGQehWPEgL",
     },
     {
-      urls: "turn:openrelay.metered.ca:443?transport=tcp",
-      username: "openrelayproject",
-      credential: "openrelayproject",
+      urls: "turn:global.relay.metered.ca:443",
+      username: "ea791fc601b5a9a0521404a8",
+      credential: "y83LlUrGQehWPEgL",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "ea791fc601b5a9a0521404a8",
+      credential: "y83LlUrGQehWPEgL",
     },
   ],
 };
